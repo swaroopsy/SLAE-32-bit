97ab202e11761091fa2b451f1207bfeb0ec6fd33
Swaroop Yermalkar <swaroop.sy@gmail.com>
2014-12-15 16:06:14 +0530

    update

--- 1. bind-tcp/Bind-Shell.c
+++ 1. bind-tcp/Bind-Shell.c
@@ -21,7 +21,7 @@ main(int argc, char **argv)
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 		
 	server.sin_family = AF_INET;
-	server.sin_port = htons(4444);
+	server.sin_port = htons(4444);  //Using 4444 port
 	server.sin_addr.s_addr = INADDR_ANY;
 			
 	bind(sock, (struct sockaddr *)&server, sockaddr_len);
